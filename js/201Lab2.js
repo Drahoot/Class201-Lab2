a348ed97ff0cda52e21440b34214ccbced9a22db
Ryan <superryanman1@gmail.com>
2022-02-09 16:38:15 -0800

    added more into my html, css and Js file

--- js/201Lab2.js
+++ js/201Lab2.js
@@ -2,6 +2,7 @@
 
 console.log('hey world');
 
+let counter = 0;
 let userName = prompt('What is your name?');
 alert(`Welcome ${userName} to my page! Since you're here, we're gonna play a yes or no game!`);
 
@@ -11,7 +12,8 @@ let answerOne = prompt('Do I have a cat?').toLowerCase();
 // .toUpperCase must have yes no answers be all capitals
 
   if(answerOne === 'yes' || answerOne === 'y'){
-  alert ('Yes I do have a cat')
+  alert ('Yes I do have a cat');
+  counter = counter + 1;
   } else if(answerOne === 'no' || answerOne === 'n'){
   alert('Sorry you are incorrect')
   }
@@ -24,12 +26,14 @@ let answerTwo = prompt('Do I play the bass?').toLowerCase();
   alert('Unfortunatly I do not, I play the guitar!');
   } else if(answerTwo === 'no' || answerTwo === 'n'){
   alert('Correct! I play the Guitar');
+  counter = counter + 1;
   }
   console.log(`${userName} answered ${answerTwo} for question Two`);
 
 let answerThree = prompt('Is Ryans favorite genre of music Metal?').toLowerCase();
   if(answerThree === 'yes' || answerThree === 'y'){
     alert('Thats correct! Ryan loves his metal!');
+    counter = counter + 1;
   } else if(answerThree === 'no' || answerThree === 'n'){
     alert('What???? Of course Ryan loves metal!');
   }
@@ -38,6 +42,7 @@ let answerThree = prompt('Is Ryans favorite genre of music Metal?').toLowerCase(
 let answerFour = prompt('Is Ryans favorite band Death?').toLowerCase();
   if(answerFour === 'yes' || answerFour === 'y'){
     alert('Oh yeah he does, that is a great band after all');
+    counter = counter + 1;
   } else if(answerFour === 'no' || answerFour === 'n'){
     alert('How could he not? That band is awesome!');
   }
@@ -48,7 +53,29 @@ let answerFive = prompt('Is this last question relavent?').toLowerCase();
     alert('No... It most definitly is not...');
   } else if(answerFive === 'no' || answerFive === 'n'){
     alert('Correct! Even Ryan does not know why he put in this question!');
+    counter = counter + 1;
   }
   console.log(`${userName} answered ${answerFive} for question Five`);
 
-alert(`Thank you ${userName} for playing this simple guessing game! Take care!`);
+let myNumber = '17';
+
+let i = 0;
+userNumber = prompt('What number am I thinking of? Give ya a hint, it is between One and 25');
+while (i <= 2){
+  i++;
+  //userNumber = prompt('What number am I thinking of? Give ya a hint, it is between One and 25');
+  if (userNumber > myNumber){
+      alert('Sorry bro, too high of a number');
+      userNumber = prompt('What number am I thinking of? Give ya a hint, it is between One and 25');
+  } else if (userNumber < myNumber){
+      alert('Sorry bro, too low of a number');
+      userNumber = prompt('What number am I thinking of? Give ya a hint, it is between One and 25');
+  } else {
+      alert('You are correct!');
+      counter = counter + 1;
+      //console.log(userName, userNumber)
+      break;
+  }
+}
+alert('The correct answer was 17, Better luck next time!')
+alert(`Thank you ${userName} for playing this simple guessing game! You have gotten ${counter} answers correct!`);
